53f0f0375a5de78477347ce02a0ecd51a9128178
Mark Bence Kiss <bassexplorer.beni@gmail.com>
2020-12-18 16:02:44 +0100

    Fix Linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,39 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "comma-dangle": ["error", "only-multiline"],
+    "linebreak-style": "off",
+    "object-curly-newline": "off",
+    "prettier/prettier": ["error", {
+     "endOfLine":"auto",
+     "disableLanguages": ["html"]
+   }],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always'
+      }
+    ],
+    'vue/html-indent': [
+      'error',
+      2,
+      {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: ['pre', 'textarea', 'span']
+      }
+    ],
+    // 'vue/singleline-html-element-content-newline': [
+    //   'error',
+    //   {
+    //     ignoreWhenNoAttributes: true,
+    //     ignoreWhenEmpty: true,
+    //     ignores: ['pre', 'textarea', 'span']
+    //   }
+    // ]
   }
 };
